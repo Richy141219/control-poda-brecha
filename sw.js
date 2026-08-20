99b5f4d4e6551b810cb701da4545b2b3fe24773e
Ricardo <ricardo@example.com>
2026-08-20 10:51:46 -0600

    Replace header mark with CFE logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "control-poda-brecha-v5";
+const CACHE_NAME = "control-poda-brecha-v6";
 const APP_SHELL = [
   "/",
   "/index.html",
@@ -7,6 +7,7 @@ const APP_SHELL = [
   "/brecha-data.js",
   "/manifest.webmanifest",
   "/icon.svg",
+  "/assets/cfe-logo.png",
   "/vendor/leaflet.css",
   "/vendor/leaflet.js",
   "/vendor/xlsx.full.min.js",
